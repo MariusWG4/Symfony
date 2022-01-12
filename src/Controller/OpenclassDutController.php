60c31e9a5d84c8a0dee158bcc39d7c69e36e93ed
MariusWG4 <74535067+MariusWG4@users.noreply.github.com>
2022-01-12 18:53:40 +0100

    Add files via upload

--- src/Controller/OpenclassDutController.php
+++ src/Controller/OpenclassDutController.php
@@ -5,18 +5,52 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 class OpenclassDutController extends AbstractController
 {
-    /**
-     * @Route("/openclass/dut", name="openclass_dut")
+   /**
+     * @Route("/", name="Accueil")
      */
     public function index(): Response
     {
-        return $this->render('openclass_dut/index.html.twig', [
-            'controller_name' => 'OpenclassDutController',
-        
-               
-        ]);
+        return $this->render('openclass_dut/index.html.twig');
     }
-}
+
+
+    /**
+     * @Route("/entreprises", name="entreprises")
+     */
+    public function afficherEntreprises(): Response
+    {
+        $repositoryStage = $this->getDoctrine()->getRepository(Stage::class);
+
+        // Récupérer les ressources enregistrées en BD
+        $stages = $repositoryStage->findAll();
+        return $this->render('openclass_dut/afficherEntreprises.html.twig',['stages'=>$stages]);}
+    /**
+     * @Route("/formations", name="formations")
+     */
+    public function afficherFormations(): Response
+    {
+        $repositoryFormation = $this->getDoctrine()->getRepository(Formation::class);
+
+        // Récupérer les ressources enregistrées en BD
+        $formations = $repositoryFormation->findAll();
+        return $this->render('openclass_dut/afficherFormations.html.twig',['formations' => $formations]);
+    }
+    /**
+     * @Route("/stages", name="openclass_dut")
+     */
+    public function afficherDescriptifStage($id): Response
+    {
+        
+        $repositoryStage = $this->getDoctrine()->getRepository(Stage::class);
+
+        // Récupérer les ressources enregistrées en BD
+        $stage = $repositoryStage->find($id);
+        return $this->render('openclass_dut/afficherDescriptifStages.html.twig', 
+        ['stage' => $stage]);
+    }
+   
+   
+    }
+
